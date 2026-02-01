8d4a39c2c60c7962b7f596db74c2fa25a3dbdadc
obinnadavid144-jpg <obinnadavid144@gmail.com>
2026-02-01 18:43:26 +0100

    Initialize App component with layout structure

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div className="flex h-screen bg-gray-900 text-white">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main content */}
+      <div className="flex-1 flex flex-col">
+        <Topbar />
+        <Dashboard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
