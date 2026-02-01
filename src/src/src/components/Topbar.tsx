b008bda4ee02a094f4daa3df924f988d06b3d749
obinnadavid144-jpg <obinnadavid144@gmail.com>
2026-02-01 19:16:34 +0100

    Display BTC price in Topbar component

--- src/src/src/components/Topbar.tsx
+++ src/src/src/components/Topbar.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import BTCIcon from "./icons/BTCIcon";
+import { useTradingStore } from "../store/useTradingStore";
 
 export default function Topbar() {
+  const { btcPrice } = useTradingStore();
+
   return (
     <div className="flex justify-between items-center bg-gray-800 p-4 border-b border-gray-700">
-      {/* Left: Current symbol */}
-      <div className="text-xl font-semibold">
+      {/* Left: BTC Symbol + Icon */}
+      <div className="flex items-center gap-2 text-xl font-semibold">
+        <BTCIcon size={24} />
         BTC / USDT
       </div>
 
       {/* Right: Fake user info */}
       <div className="flex items-center gap-4">
-        <div className="text-green-400 font-bold">$10,000</div>
+        <div className="text-green-400 font-bold">${btcPrice.toFixed(2)}</div>
         <div className="w-8 h-8 bg-gray-600 rounded-full flex items-center justify-center text-white">
           U
         </div>
